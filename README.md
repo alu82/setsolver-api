f11862206eaa09d0f7533b287d2068e94997f218
alu82 <ldw_srv@posteo.de>
2021-08-28 21:12:20 +0200

    find sets and pass them through the api (first dirty version)

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ The API for the the SET solver.
 Idea: 
 - Gets an image of (not overlapping) set cards and detects valid sets.
 - API implemented with Flask and model trained with pytorch (see setsolver-card-classifier project)
+
+## run locally
+```
+export FLASK_APP=setsolverapi
+flask run --host=0.0.0.0
+```
--- setsolverapi/api.py
+++ setsolverapi/api.py
@@ -8,9 +8,19 @@ from PIL import Image
 import numpy as np
 import time
 import os
+import itertools as it
 
 from setsolverapi import app
 from setsolverapi.classifier import classifier as clf
+
+cards = dict()
+card_id = 0
+for number in range(3):
+    for color in range(3):
+        for form in range(3):
+            for filling in range(3):
+                cards[card_id] = (number, color, form, filling)
+                card_id += 1
 
 @app.route("/")
 def root():
@@ -30,7 +40,30 @@ def solve():
         prediction["probability"] = probability
         predictions.append(prediction)
 
-    return jsonify(predictions)
+    found_sets = find_valid_set([p["card"] for p in predictions])
+
+    response = {}
+    response["extractedCards"] = predictions
+    response["foundSets"] = found_sets
+    return jsonify(response)
+
+def find_valid_set(cards):
+    valid_sets = list()
+    for triple in it.combinations(cards, 3):
+        if is_valid_set(triple):
+            valid_sets.append(triple)
+    return valid_sets
+
+def is_valid_set(triple):
+    card_0 = cards[triple[0]]
+    card_1 = cards[triple[1]]
+    card_2 = cards[triple[2]]
+
+    for idx in range(len(card_0)):
+        diff_values = set([card_0[idx], card_1[idx], card_2[idx]])
+        if len(diff_values) == 2:
+            return False
+    return True
 
 def get_cards(image):
     im = transform_pil_to_opencv(image)
